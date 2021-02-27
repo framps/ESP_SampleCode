373ed77a010d6afe9d0b145ea5ebe1d54d65597c
framp <framp@linux-tips-and-tricks.de>
2021-02-27 15:07:00 +0100

    Added some more comments

--- BlinkNotification/BlinkNotification.ino
+++ BlinkNotification/BlinkNotification.ino
@@ -2,14 +2,14 @@
 
 using namespace framp;
 
-Ticker createError;             // ticker to simulate an error situation
+Ticker createError;                                 // ticker to simulate an error situation
 bool errorOccured = false;
 void setError() {
   errorOccured = true;
 }
 
-BlinkNotification sos(BUILTIN_LED,500);            // error blink pattern
-BlinkNotification working(BUILTIN_LED,100);        // active blink pattern
+BlinkNotification working(BUILTIN_LED,100);        // active blink pattern to signal ESP is active working
+BlinkNotification sos(BUILTIN_LED,500);            // error blink pattern to signal an error condition
  
 void setup() {
   
@@ -19,20 +19,20 @@ void setup() {
   sos.setup();
 
   working.setup();
-  working.start(". ",-1);
+  working.start(". ",-1);                           // -1 will blink the pattern forever
   Serial.println("Working ...");
 
-  createError.once_ms(5000, setError);
+  createError.once_ms(5000, setError);              // activate error simulation after 5 seconds
 
 }
 
 void loop() {  
 
   if ( errorOccured ) {
-    working.stopForce();
-    errorOccured = false;
-    Serial.println("SOS blinks...");
-    sos.start("... --- ...   ",-1);
+    working.stopForce();                            // terminate working blink pattern
+    errorOccured = false;   
+    Serial.println("SOS blinks...");              
+    sos.start("... --- ...   ",-1);                 // blink the error pattern (SOS)
   }
   
 }
