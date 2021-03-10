36ec349d91835f765adc066991a4209ead11b6f0
framp <framp@linux-tips-and-tricks.de>
2021-03-10 17:02:28 +0100

    Added ESPNow code. Sample complete now

--- DHT22-BME280-espnow-deepSleep-sensor/TempHumSensor.h
+++ DHT22-BME280-espnow-deepSleep-sensor/TempHumSensor.h
@@ -1,3 +1,26 @@
+/*
+#######################################################################################################################
+#
+#    Copyright (c) 2021 framp at linux-tips-and-tricks dot de
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################################################################
+*/
+
+// Latest code available on https://github.com/framps/ESP_stuff/tree/main/DHT22-BME280-espnow-deepSleep-sensor
+
 #pragma once
 
 	class Sensor {
@@ -12,9 +35,9 @@
     Sensor() {};      
     virtual ~Sensor() { };
       
-    virtual int start() = 0;                          // rc 0 -> OK, fail otherwise    
-    virtual int poll(Data& polledData) = 0;           // rc 0 -> OK, fail otherwise
-
+    virtual int start() = 0;                          // rc 0 -> request failed
+    virtual int poll(Data& polledData) = 0;           // rc 0 -> request failed
+    virtual const char* name() = 0;
     };
 
   #include "DHT.h"
@@ -27,8 +50,9 @@
      
     virtual ~DHT22Sensor() { };
       
-    int start();                                 // rc 0 -> OK, fail otherwise    
-    int poll(Sensor::Data& polledData);          // rc 0 -> OK, fail otherwise
+    int start();                                 // rc 0 -> request failed   
+    int poll(Sensor::Data& polledData);          // rc 0 -> request failed
+    const char* name() { return "DHT22"; };
 
   private:
     DHT dht;
@@ -46,8 +70,9 @@
     BME280Sensor (uint8_t powerPin);      
     virtual ~BME280Sensor () { };
       
-    int start();                                 // rc 0 -> OK, fail otherwise    
-    int poll(Sensor::Data& polledData);          // rc 0 -> OK, fail otherwise
+    int start();                                 // rc 0 -> request failed
+    int poll(Sensor::Data& polledData);          // rc 0 -> request failed
+    const char* name() { return "BME280"; };
 
   private:
     BME280I2C bme;
